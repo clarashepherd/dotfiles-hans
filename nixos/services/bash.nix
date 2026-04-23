c73f6fb084b07a25e171b6f3fd927a8d95fc59be
Clara Shepherd <clsh@bang-olufsen.dk>
2026-04-23 08:58:12 +0200

    nixos: personalize .bashrc
    Added a few alias, renamed a file.

--- nixos/services/bash.nix
+++ nixos/services/bash.nix
@@ -4,17 +4,27 @@
   programs.bash = {
         enable = true;
         interactiveShellInit = ''
-            if [ -f "$HOME/.secrets/github_token" ]; then
-                export GITHUB_TOKEN="$(cat $HOME/.secrets/github_token)"
+            if [ -f "$HOME/.secrets/github-token" ]; then
+                export GITHUB_TOKEN="$(cat $HOME/.secrets/github-token)"
             fi
             if [ -d "$HOME/repos/beo-scripts" ]; then
                 export PATH="$HOME/repos/beo-scripts:$PATH"
+		if [ -f "$HOME/repos/beo-scripts/aliases.sh" ]; then
+                    source "$HOME/repos/beo-scripts/aliases.sh"
+                fi
             fi
+            if [ -f /usr/share/git/completion/git-completion.bash ]; then
+                source /usr/share/git/completion/git-completion.bash
+            fi
+
+
         '';
         shellAliases = {
-            ls = "eza -F";
-            la = "eza -la";
             grep = "grep --color=auto";
+	    ls = "ls  -a --color=auto";
+            ff = "fzf";
+            lg = "lazygit";
+            gg = "git grep --color";
         };
         promptInit = ''
           PS1="\[\e]2;\h:\u:\w\a\]\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ "
