d63b9b9466cf91b0be7cedeb58d1b757f5344c88
Clara Shepherd <clsh@bang-olufsen.dk>
2026-08-02 17:03:06 +0200

    nixos: add flake, switch to unstable releases
    Introduced some breaking changes for e.g. hyprland. This commit also
    resolves those.
    
    Signed-off-by: Clara Shepherd <clsh@bang-olufsen.dk>

---
+++ nixos/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Clara's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+}
--- nixos/services/kernel.nix
+++ nixos/services/kernel.nix
@@ -1,33 +1,4 @@
 { pkgs, lib, ... }:
-
-let
-  linux = pkgs.buildLinux rec {
-    version = "6.18.26";
-    modDirVersion = version;
-
-    src = fetchTarball {
-      url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
-      sha256 = "sha256:0ahcx78v71nspmyyhcb0rb27dv52apn531g9gxs4zscx3s1mrnfi";
-    };
-
-    # Inherit sensible defaults from the current kernel packages
-    kernelPatches = [];
-    structuredExtraConfig = {};
-  };
-
-  kernelPackages = pkgs.linuxPackagesFor linux;
-in
 {
-  boot.kernelPackages = kernelPackages;
-
-  boot.kernelPatches = [
-    {
-      name = "v7_20251216_bin_du_add_amd_isp4_driver";
-      patch = pkgs.fetchurl {
-        url = "https://lore.kernel.org/all/20251216091326.111977-1-Bin.Du@amd.com/t.mbox.gz";
-        # hash can change due to mailing list stuff...
-        hash = "sha256-ncKvOyV8YzGJR0XdyspcH2tlanYeYnV68EstYcMax+o=";
-      };
-    }
-  ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
