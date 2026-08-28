c1b546c74b075d90c7114d9012fcfe0c2f152c35
Clara Shepherd <clsh@bang-olufsen.dk>
2026-08-28 09:09:24 +0200

    nixos: add openconnect workaround
    Openconnect is built using an old version of python and needs a patch.
    
    Hopefully I can remove once it's fixed upstream.
    
    Signed-off-by: Clara Shepherd <clsh@bang-olufsen.dk>

--- nixos/services/openconnect.nix
+++ nixos/services/openconnect.nix
@@ -1,6 +1,15 @@
 { config, pkgs, ... }:
 
 let
+  setuptools-legacy = pkgs.python3Packages.setuptools.overridePythonAttrs (old: rec {
+    version = "80.10.2";
+    src = pkgs.fetchPypi {
+      pname = "setuptools";
+      inherit version;
+      hash = "sha256-iw6dEMeEv30mLE5exdTslBJ84gboc48ppDeUX7whm3A=";
+    };
+  });
+
   openconnect-sso = pkgs.python3Packages.buildPythonApplication {
     pname = "openconnect-sso";
     version = "0-unstable-2024-04-22";
@@ -17,6 +26,11 @@ let
       substituteInPlace pyproject.toml \
         --replace-fail 'requires = ["poetry>=0.12"]' 'requires = ["poetry-core>=1.0.0"]' \
         --replace-fail 'build-backend = "poetry.masonry.api"' 'build-backend = "poetry.core.masonry.api"'
+      # https://github.com/vlaci/openconnect-sso/issues/207
+      substituteInPlace openconnect_sso/app.py \
+        --replace-fail \
+          'asyncio.get_event_loop().run_until_complete(' \
+          'asyncio.new_event_loop().run_until_complete('
     '';
 
     nativeBuildInputs = with pkgs.python3Packages; [
@@ -36,7 +50,7 @@ let
       requests
       structlog
       toml
-      setuptools
+      setuptools-legacy
       pysocks
       pyqt6
       pyqt6-webengine
